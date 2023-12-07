9bf38143c73ec7815e39c03ad05f1a63bb5e0273
rat <153201297+rat960@users.noreply.github.com>
2023-12-07 16:07:30 +0200

    Added main script
    thats all

---
+++ window.bat
@@ -0,0 +1,35 @@
+<!-- :
+@echo off
+mshta.exe "%~f0"
+exit /b %errorlevel%
+-->
+
+<html>
+<head>
+	<title>Window</title>
+</head>
+<style>
+p {
+    font-family: sans-serif;
+    text-align: center;
+}
+h2 {
+    font-family: sans-serif;
+    text-align: center;
+}
+</style>
+<body>
+	<h2>Your title</h2>
+	<br>
+	<p>this is a simple window!</p>
+
+<button>Button</button>
+
+
+    <script language='javascript' type="text/javascript">
+     window.resizeTo(900,700)
+     </script>
+
+</body>
+
+</html>
